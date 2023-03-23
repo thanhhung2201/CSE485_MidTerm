6eb19817031b8261d31146a9344121b414ecac8a
hung <nguyenhunglt2201@gmail.com>
2023-03-23 15:44:31 +0700

    test

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $data = Employee::latest()->paginate(5);
+
+        return view('index', compact('data'))->with('i', (request()->input('page', 1) - 1) * 5);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'employee_name'          =>  'required',
+            'employee_place'         =>  'required',
+            'employee_salary'         =>  'required',
+            'employee_office'         =>  'required',
+        ]);
+
+        
+
+        $employee = new Employee;
+
+        $employee->employee_name = $request->employee_name;
+        $employee->employee_place = $request->employee_place;
+        $employee->employee_salary = $request->employee_salary;
+        $employee->employee_office = $request->employee_office;
+
+        $employee->save();
+
+        return redirect()->route('employees.index')->with('success', 'Student Added successfully.');
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employee $employee)
+    {
+        //
+        return view('show', compact('employee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Employee $employee)
+    {
+        //
+        return view('edit', compact('employee'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //
+        $request->validate([
+            'employee_name'          =>  'required',
+            'employee_place'         =>  'required',
+            'employee_salary'         =>  'required',
+            'employee_office'         =>  'required',
+        ]);
+
+        
+
+        $employee = new Employee;
+
+        $employee = Employee::find($request->name);
+        $employee->employee_place = $request->employee_place;
+        $employee->employee_salary = $request->employee_salary;
+        $employee->employee_office = $request->employee_office;
+
+        $employee->save();
+        return redirect()->route('employees.index')->with('success', 'Student Data has been updated successfully.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee $employee)
+    {
+        //
+        $employee->delete();
+        return redirect()->route('employees.index')->with('success', 'Student Data  deleted successfully.');
+    }
+}
---
+++ resources/views/show.blade.php
@@ -0,0 +1,39 @@
+@extends('master')
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <div class="row">
+            <div class="col col-md-6"><b>Employee Details</b></div>
+            <div class="col col-md-6">
+                <a href="{{ route('employees.index') }}" class="btn btn-primary btnsm float-end">View All</a>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="row mb-3">
+            <label class="col-sm-2 col-label-form"><b>Employee Name</b></label>
+            <div class="col-sm-10">
+                {{ $employee->name }}
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-2 col-label-form"><b>Employee Place</b></label>
+            <div class="col-sm-10">
+                {{ $employee->email }}
+            </div>
+        </div>
+        <div class="row mb-4">
+            <label class="col-sm-2 col-label-form"><b>Employee Salary</b></label>
+            <div class="col-sm-10">
+                {{ $employee->salary }}
+            </div>
+        </div>
+        <div class="row mb-4">
+            <label class="col-sm-2 col-label-form"><b>Employee Office</b></label>
+            <div class="col-sm-10">
+                {{ $employee->office }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection('content')
